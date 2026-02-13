396db58559f248bc0fde508ddad4bb5a88ebe7a2
sashimarconi <juanmarconi10@gmail.com>
2026-02-12 21:36:52 -0300

    k

--- api/admin/[[...path]].js
+++ api/admin/[[...path]].js
@@ -1,5 +1,10 @@
-const handler = require("../admin-global-router");
-
-module.exports = async (req, res) => {
-  await handler(req, res);
+﻿module.exports = async (req, res) => {
+  try {
+    const handler = require("../admin-global-router");
+    await handler(req, res);
+  } catch (error) {
+    if (!res.headersSent) {
+      res.status(500).json({ error: error?.message || "Internal server error" });
+    }
+  }
 };
--- api/dashboard/[[...path]].js
+++ api/dashboard/[[...path]].js
@@ -1,5 +1,10 @@
-﻿const handler = require("../admin-router");
-
-module.exports = async (req, res) => {
-  await handler(req, res);
+﻿module.exports = async (req, res) => {
+  try {
+    const handler = require("../admin-router");
+    await handler(req, res);
+  } catch (error) {
+    if (!res.headersSent) {
+      res.status(500).json({ error: error?.message || "Internal server error" });
+    }
+  }
 };
